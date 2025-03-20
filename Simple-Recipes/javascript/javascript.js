ce5590c53b85460dfcde9040ae76e54c5e4b425c
Schilz-Aiden <aidenschilz5@gmail.com>
2025-03-20 14:04:39 -0600

    Add files via upload

--- Simple-Recipes/javascript/javascript.js
+++ Simple-Recipes/javascript/javascript.js
@@ -11,20 +11,28 @@ var closeBtn = document.getElementsByClassName("close-btn");
 //When the user clicks the recipe button, open the modal
 for (var i = 0; i < btns.length; i++) {
     btns[i].onclick = function (event) {
-        modal = document.querySelector(event.target.getAttribute
-        ("href"));
+        modal = document.querySelector(event.target.getAttribute("href"));
         modal.style.display = "block";
+
+        // Set a timeout to close the modal after 10 seconds (10000ms)
+        modalTimeout = setTimeout(function () {
+            modal.style.display = "none";
+        }, 10000);
+
     }
 }
 
-//When the user clicks on <span> (x), close the modal
-for (var i = 0; i <closeBtn.length; i++) {
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < closeBtn.length; i++) {
     closeBtn[i].onclick = function () {
         for (var index in modals) {
             if (modals[index].style) {
                 modals[index].style.display = "none";
             }
         }
+
+        // Clear the timeout when manually closing the modal
+        clearTimeout(modalTimeout);
     }
 }
 
