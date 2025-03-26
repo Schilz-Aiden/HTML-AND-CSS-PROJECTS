26c71f2636c9459a714a3205c2852c646e058a0f
Schilz-Aiden <aidenschilz5@gmail.com>
2025-03-26 11:47:57 -0600

    Add files via upload

--- Academy Cinemas/javascript/javascript.js
+++ Academy Cinemas/javascript/javascript.js
@@ -18,7 +18,7 @@ const toastElList = document.querySelectorAll('.toast')
 const toastList = [...toastElList].map(toastEl => 
     new bootstrap.Toast(toastEl))
 
-    
+
 //Function to display toast with selected options
 function displaySelectedMovieOptions() {
     var movie = document.getElementById('movieSelect').options[document.getElementById('movieSelect').selectedIndex].text;
@@ -37,3 +37,39 @@ function displaySelectedMovieOptions() {
 function buyTickets(){
     displaySelectedMovieOptions();
 }
+
+//JQuery
+
+//Shrinks header size when the document is scrolled down by 80 pixels
+$(document).on("scroll", function() {
+    //When the webpage is scrolled down from the top by 50px this
+    //if statement will trigger
+    if ($(document).scrollTop() > 50) {
+        //Once the 50px requirement has been met add the
+        //nav-shrink class selector to the same HTML element
+        //that has the nav class
+        $("nav").addClass("nav-shrink");
+        //Adjusts the position of the mobile drop menu
+        //to accommodate the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //if the webpage has not been scrolled down or
+        //is back at the top the nav-shrink class selector
+        //is removed from the HTML element with the nav
+        //class selector
+        $("nav").removeClass("nav-shrink");
+        //The margin for the drop menu is now
+        //returned to it's original amount
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
+
+//Close mobile menu when a navigation link is clicked
+$(document).ready(function () {
+    //On click when and element contains just the nav-link class and not the dropdown-toggle and then
+    //also close when an element with the class /droppdown-item (each movie link) has been clicked
+    $(".navbar-nav").on('click', 'nav-link:not(".dropdown-toggle"), .dropdown-item', function() {
+        //Collapse the navbar when a link or dropdown item is clicked
+        $(".navbar-collapse").collapse('hide');
+    });
+});
